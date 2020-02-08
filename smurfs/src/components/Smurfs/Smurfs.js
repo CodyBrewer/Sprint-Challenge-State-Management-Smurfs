c2e089b029ecfb5c543dad43440b20a827d5328f
Cody Brewer <brewer.cody.m@gmail.com>
2020-02-08 12:08:57 -0700

    added SmurfForm

--- smurfs/src/components/Smurfs/Smurfs.js
+++ smurfs/src/components/Smurfs/Smurfs.js
@@ -1,6 +1,6 @@
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchSmurfs } from "../../store/actions";
+import { fetchSmurfs, addSmurf } from "../../store/actions";
 
 const Smurfs = () => {
   const dispatch = useDispatch();
@@ -17,6 +17,7 @@ const Smurfs = () => {
       ) : (
         <p>no smurfs here</p>
       )}
+      <SmurfForm />
     </>
   );
 };
@@ -27,9 +28,58 @@ const Smurf = props => {
   return (
     <>
       <h2>{smurf.name}</h2>
-      <p>Age: {smurf.age}</p>
+      <p>Age: {smurf.age} years</p>
       <p>Height: {smurf.height}</p>
     </>
+  );
+};
+
+const SmurfForm = () => {
+  const dispatch = useDispatch();
+  const [smurfInfo, setSmurfInfo] = useState({ name: "", age: 0, height: 0 });
+  const submitSmurf = e => data => {
+    e.preventDefault();
+
+    dispatch(addSmurf(data));
+  };
+
+  const onChange = e => {
+    setSmurfInfo({ ...smurfInfo, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <form
+      onSubmit={() => {
+        submitSmurf(smurfInfo);
+      }}
+    >
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Name of Smurf"
+        value={smurfInfo.name}
+        onChange={onChange}
+      />
+      <input
+        type="number"
+        name="age"
+        id="age"
+        placeholder="Age of Smurf in years"
+        value={smurfInfo.age}
+        onChange={onChange}
+      />
+      <input
+        type="number"
+        name="height"
+        id=""
+        placeholder="Height of Smurf in cm
+        "
+        value={smurfInfo.height}
+        onChange={onChange}
+      />
+      <button type="submit">Add Smurf!</button>
+    </form>
   );
 };
 
